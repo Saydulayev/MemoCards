b962f11199c852916664a6d95612b513f213d0a5
Saydulayev <saydulayev.wien@gmail.com>
2025-01-25 10:51:16 +0100

    Initial Commit

---
+++ MemoCards/MemoCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoCardsApp.swift
+//  MemoCards
+//
+//  Created by Saydulayev on 25.01.25.
+//
+
+import SwiftUI
+
+@main
+struct MemoCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
